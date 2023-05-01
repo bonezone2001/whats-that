addc6d76491b06732e2a55305a3d585fdbd5d07c
Kyle Pelham <kylepelham9000@gmail.com>
2023-05-01 05:09:02 +0100

    adhere to airbnb + delete placeholder data

--- styles/navigators.js
+++ styles/navigators.js
@@ -1,11 +1,13 @@
 import Avatar from '@components/shared/avatar';
 import { Ionicons } from '@expo/vector-icons';
-import { colors } from './global_style';
 import { useStore } from '@store';
+
+import React from 'react';
+import { colors } from './global_style';
 
 export const screenOptions = {
     headerShown: false,
-    animation: "fade_from_bottom",
+    animation: 'fade_from_bottom',
 };
 
 export const headerOptions = {
@@ -25,16 +27,24 @@ export const headerOptions = {
 // TODO: Move this to authorized_bottom_tab.js
 export const tabBarOptions = ({ route }) => {
     const store = useStore();
-
     return {
         tabBarIcon: ({ focused, color, size }) => {
             let iconName;
-            if (route.name === 'Profile')
-                return <Avatar size={30} style={{ opacity: focused ? 1 : 0.5, borderWidth: 1, borderColor: 'grey' }} source={{ uri: store?.user?.avatar }} />
-            else if (route.name === 'Chat')
-                iconName = focused ? 'chatbox-ellipses' : 'chatbox-ellipses-outline';
-            else if (route.name === 'Contacts')
-                iconName = focused ? 'people' : 'people-outline';
+            if (route.name === 'Profile') {
+                return (
+                    <Avatar
+                        size={30}
+                        style={{
+                            opacity: focused ? 1 : 0.5,
+                            borderWidth: 1,
+                            borderColor: 'grey',
+                        }}
+                        source={{ uri: store?.user?.avatar }}
+                    />
+                );
+            }
+            if (route.name === 'Chat') iconName = focused ? 'chatbox-ellipses' : 'chatbox-ellipses-outline';
+            else if (route.name === 'Contacts') iconName = focused ? 'people' : 'people-outline';
             return <Ionicons name={iconName} size={size} color={color} />;
         },
         tabBarLabel: () => null,
