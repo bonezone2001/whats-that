3c77670b817e5e93f207ec1b821df1c02684cb8a
Kyle Pelham <kylepelham9000@gmail.com>
2023-05-01 05:16:26 +0100

    refactor contacts screens with new components

---
+++ components/views/contact/contact_view.js
@@ -0,0 +1,44 @@
+import {
+    View,
+    Text,
+    FlatList,
+    Image,
+} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { contactStyle } from '@styles';
+import { useStore } from '@store';
+import api from '@api';
+
+import noResultsImage from '@assets/images/no_results.png';
+import ContactCard from '@components/shared/contact_card';
+
+export default function ContactView() {
+    const [contacts, setContacts] = useState([]);
+    const store = useStore();
+
+    useEffect(() => {
+        Promise.all(store.contacts?.map(async (contact) => {
+            const avatarData = await api.getUserPhoto(contact.user_id);
+            return { ...contact, avatar: avatarData, isContact: true };
+        }) || [])
+            .then((result) => setContacts(result));
+    }, [store.contacts]);
+
+    return (
+        <View style={contactStyle.container}>
+            {contacts.length > 0 ? (
+                <FlatList
+                    style={contactStyle.contactList}
+                    data={contacts}
+                    renderItem={({ item }) => <ContactCard type="view" contact={item} />}
+                    keyExtractor={(item) => item.user_id.toString()}
+                />
+            ) : (
+                <View style={[contactStyle.placeholderContainer, { alignItems: 'center' }]}>
+                    <Image source={noResultsImage} style={contactStyle.placeholderImage} />
+                    <Text style={contactStyle.placeholderText}>No results found</Text>
+                </View>
+            )}
+        </View>
+    );
+}
